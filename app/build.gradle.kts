14c85a063f4096682dcafdcf9a6fdf8eb85e7d0d
Mohammad Amin Haji Alirezaei <m.a.hajialirezaei05@gmail.com>
2025-08-15 01:21:05 +0330

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,44 +1,39 @@
 plugins {
-    alias(libs.plugins.android.application)
+    id 'com.android.application'
+    id 'org.jetbrains.kotlin.android' // اگه داری
 }
+
 android {
-    namespace = "ir.ac.kntu.project4"
-    compileSdk = 35
+    namespace 'com.example.yourapp'
+    compileSdk 34
 
     defaultConfig {
-        applicationId = "ir.ac.kntu.project4"
-        minSdk = 28
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        applicationId "com.example.yourapp"
+        minSdk 24
+        targetSdk 34
+        versionCode 1
+        versionName "1.0"
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        testInstrumentationRunner "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            minifyEnabled false
+            proguardFiles getDefaultProguardFile('proguard-android-optimize.txt'), 'proguard-rules.pro'
         }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    buildToolsVersion = "34.0.0"
-    ndkVersion = "27.0.11902837 rc2"
 }
 
 dependencies {
+    implementation 'androidx.core:core-ktx:1.12.0'
+    implementation 'androidx.appcompat:appcompat:1.6.1'
+    implementation 'com.google.android.material:material:1.11.0'
+    implementation 'androidx.constraintlayout:constraintlayout:2.1.4'
 
-    implementation(libs.appcompat)
-    implementation(libs.material)
-    implementation(libs.firebase.firestore)
-    testImplementation(libs.junit)
-    testImplementation(libs.junit.jupiter)
-    androidTestImplementation(libs.ext.junit)
-    androidTestImplementation(libs.espresso.core)
+    testImplementation 'junit:junit:4.13.2'
+    androidTestImplementation 'androidx.test.ext:junit:1.1.5'
+    androidTestImplementation 'androidx.test.espresso:espresso-core:3.5.1'
+
+    testImplementation 'com.puppycrawl.tools:checkstyle:10.12.1'
 }
